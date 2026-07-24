f4705d3faf361b9d7be01580353fb443b26b5022
Gowtham C <814724243047@trp.srmtrichy.edu.in>
2026-07-24 16:25:38 +0530

    Update Ex-02.py

--- Ex-02.py
+++ Ex-02.py
@@ -16,8 +16,8 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy', 
               metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=5, verbose=1)
-print("Name: Herman Joel Raj J")
-print("Reg.no: 814724243059")
+print("Gowtham C")
+print("814724243047")
 print("Accuracy:", model.evaluate(x_test, y_test, verbose=0)[1])
 pred = np.argmax(model.predict(x_test[0:1]), axis=1)
 plt.imshow(x_test[0].reshape(28, 28), cmap="gray")
